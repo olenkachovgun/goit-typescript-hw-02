9cdd1f76f4d55c0da0d9148937bb488d8e17db93
Olena_Chovgun <olenkachovgun@gmail.com>
2025-03-16 17:34:30 +0200

    fix normalize

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import "./index.css";
-// import App from './components/App/App';
+
 import { Toaster } from "react-hot-toast";
 import App from "./components/App/App";
 
