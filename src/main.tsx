1362632ada67b79cfa5211038075f4f8197b0451
Olena Chovgun <59543079+olenkachovgun@users.noreply.github.com>
2025-03-16 17:41:57 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "modern-normalize";
+import "modern-normalize";
 import "./index.css";
 
 import { Toaster } from "react-hot-toast";
